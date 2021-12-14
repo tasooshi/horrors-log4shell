4f5aa2e536b855ebc2fd0a51b0f60dd52eeb339a
Piotr Ługowski <piolug93@gmail.com>
2021-12-14 12:33:33 +0100

    merge

--- Vulnerable/src/main/java/com/example/Vulnerable.java
+++ Vulnerable/src/main/java/com/example/Vulnerable.java
@@ -19,18 +19,24 @@ public class Vulnerable {
     static final Logger logger = LogManager.getLogger(Vulnerable.class);
 
     @GetMapping("/")
-    String home(@RequestHeader(name="User-Agent") String userAgent, @RequestParam(value="somefield", defaultValue="default") String somefield) {
-        return String.format("<html><head><title>Vulnerable Application</title></head><body>Welcome!</body></html>");
+    String home() {
+        return "<html><head><title>Vulnerable Application</title></head><body>Welcome!</body></html>";
     }
 
     @RequestMapping("/endpoint")
     String endpoint(@RequestHeader(name="User-Agent") String userAgent, @RequestParam(value="somefield", defaultValue="default") String somefield) {
+<<<<<<< HEAD:Vulnerable/src/main/java/Vulnerable.java
         logger.info("User-Agent:" + userAgent.substring(1));
         logger.info("User-Agent:" + userAgent);
         return String.format("<html><head><title>Vulnerable Application</title></head><body><form><input name=\"somefield\" type=\"text\"><button type=\"submit\">Submit</button></form></body></html>");
+=======
+        logger.info("User-Agent:" + userAgent);
+        logger.info("somefield:" + somefield);
+        return "<html><head><title>Vulnerable Application</title></head><body><form><input name=\"somefield\" type=\"text\"><button type=\"submit\">Submit</button></form></body></html>";
+>>>>>>> 5116428 (Development related):Vulnerable/src/main/java/com/example/Vulnerable.java
     }
 
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) {
         SpringApplication.run(Vulnerable.class, args);
     }
 
