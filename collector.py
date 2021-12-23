96369d0320772a36da99b6e8fbe0a32681416097
tasooshi <tasooshi@pm.me>
2021-12-23 19:38:23 +0100

    Update related to horrors v0.6

--- collector.py
+++ collector.py
@@ -12,7 +12,6 @@ if __name__ == "__main__":
     httpd.add_route('/', ['GET'], '<html><head><title>horrors-log4shell</title></head><body><h1>Welcome</h1><hr></body></html>')
     httpd.add_route('/collect/', ['GET', 'POST'], httpd.collect)
     
-    story = scenarios.Scenario()
+    story = scenarios.Scenario(debug=True)
     story.add_service(httpd)
-    story.set_debug()
     story.play()
